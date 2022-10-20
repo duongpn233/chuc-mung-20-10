87af4aab18743544c217d5194ced2365799ba21e
NgocDuong <phamduongx9@gmail.com>
2022-10-20 22:24:53 +0700

    first commit

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
-const nameGirl = 'Em';
-const giftUrl = 'http://nodemy.vn';
-const eventName = 'Chúc Mừng 20-10';
-const titleCard = 'Tặng người ấy';
-const contentCard = 'Chúc honey của anh 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const nameGirl = '';
 
-// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
-// Cần hỗ trợ hãy liên hệ: 
-// Mr-Nam http://facebook.com/nam.nodemy
-// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hot-girl.png';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Gửi tặng một nữa thế giới';
+const contentCard = 'Chúc một nữa thế giới có một ngày đặc biệt thật ý nghĩa <3';
+
+const giftImage = 'hoa.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
